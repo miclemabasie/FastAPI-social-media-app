738d5bc8aef309445ca9edddf1c8c6af7732b234
miclemabasie <miclemabasie3@gmail.com>
2024-01-24 23:08:43 +0100

    Authorization on personal changes for posts

--- app/routers/posts.py
+++ app/routers/posts.py
@@ -22,6 +22,7 @@ def get_posts(db: Session = Depends(get_db), user_id: int = Depends(get_current_
 def get_post_detail(id: int, db: Session = Depends(get_db), user_id: int = Depends(get_current_user)) -> PostResponse:
     post = db.query(models.Post).filter(models.Post.id ==id).first() 
     if post is None:
+        print("Post not found")
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post with id not found")
     return post
 
@@ -31,7 +32,8 @@ def create_post(post: PostCreate, db: Session = Depends(get_db), user_id: int = 
     # new_post = models.Post(title=post.title, content=post.content, published=post.published)    
     # OR
     print(user_id)
-    new_post = models.Post(**post.model_dump())
+    print("This is the new post: ##########: ", user_id.id)
+    new_post = models.Post(**post.model_dump(), owner_id=user_id)
     db.add(new_post)
     db.commit()
     db.refresh(new_post) # return the new post that was just created
@@ -42,19 +44,26 @@ def create_post(post: PostCreate, db: Session = Depends(get_db), user_id: int = 
 def update_post(id: int, post: PostCreate, db: Session = Depends(get_db), user_id: int = Depends(get_current_user)) -> PostResponse:
     # Try to fetch the post with the given ID from the database
     post_query = db.query(models.Post).filter(models.Post.id == id)
-    if post is None:
+    post_object = post_query.first()
+    if post_object is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post with given id not found")
+    if post_object.owner_id != user_id.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action.")
     post_query.update(post.model_dump(), synchronize_session=False)
+    print("user credentials: ", user_id.id, post_object.owner_id)
     db.commit()
-    post_obj = post_query.first()
-    return post_obj
+    return post_object
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db), user_id: int = Depends(get_current_user)):
     post = db.query(models.Post).filter(models.Post.id == id)
     if post.first() is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post with given id not found")
+    if post.first().owner_id != user_id.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action.")
+
     post.delete(synchronize_session=False)
+    
     db.commit()
     
     return Response(status_code=status.HTTP_204_NO_CONTENT)
